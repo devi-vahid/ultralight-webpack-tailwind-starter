4fe8641c3aa8daefc2dce6599023f87d011af08d
Vahid <vahid.m.dev@gmail.com>
2022-07-05 14:17:23 +0430

    Remove unnecessary vendor.js file Added support for pdf files in webpack.common Tailwind container is properly styled now

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   darkMode: 'media',
   theme: {
     extend: {},
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+      center: true,
+    },
   },
   plugins: [],
 }
--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,6 @@ var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    'assets/scripts/vendor': "./src/assets/scripts/vendor.js",
     'assets/scripts/main': "./src/index.js",
   },
   module: {
@@ -11,6 +10,10 @@ module.exports = {
       {
         test: /\.html$/,
         use: ["html-loader"]
+      },
+      {
+        test: /\.pdf$/,
+        use: ["file-loader?minetype=application/pdf&name=static/[name].pdf"]
       },
       {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
